6e337573d84872e5177293801239e133bb14aa1f
daniiul <dmalakhov2005@gmail.com>
2024-10-24 23:04:30 -0700

    worked on implementing read race

--- main.cpp
+++ main.cpp
@@ -4,10 +4,62 @@
 #include <set>
 #include <vector>
 #include <iomanip>
+#include <string>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
 
+    ifstream inputFile("codes.txt");
+
+    if(!inputFile.is_open())
+    {
+        cout << "Error! Can't open File!";
+    }
+
+    vector<string> timeRead;
+    vector<string> timeSort;
+    vector<string> timeInsert;
+    vector<string> timeDelete;
+
+    for(int i = 0; i < 3; i++)
+    {
+        // Start timing
+        auto start = high_resolution_clock::now();
+
+        // Example loop to measure
+        if (i == 0)
+        {
+            vector<string> in;
+            string temp;
+            while(getline(inputFile, temp))
+                in.push_back(temp);
+        }
+        if (i == 1)
+        {
+            list<string> in;
+            string temp;
+            while(getline(inputFile, temp))
+                in.push_back(temp);
+        }
+        if (i == 2)
+        {
+            set<string> in;
+            string temp;
+            while(getline(inputFile, temp))
+                in.insert(temp);
+        }
+
+
+        // End timing
+        auto end = high_resolution_clock::now();
+
+        // Calculate duration
+        auto duration = duration_cast<milliseconds>(end - start);
+
+        timeRead.push_back(duration.count());
+
+    }
 
     return 0;
 }
